7e9b5c242aca683d12d4ae2516cfab92f4cfe1fd
deluvsushi <77536370+deluvsushi@users.noreply.github.com>
2022-01-25 13:37:13 +0600

    Update main_functions.py

--- configs/main_functions.py
+++ configs/main_functions.py
@@ -1,37 +1,41 @@
-import aminofix
+import amino
 
 def spam_blogs(title: str, content: str):
-	client = aminofix.Client()
-	client.login(email=input("Email >> "), password=input("Password >> "))
+	client = amino.Client()
+	email = input("-- Email::: ")
+	password = input("-- Password::: ")
+	client.login(email=email, password=password)
 	clients =  client.sub_clients(start=0, size=1000)
 	for x, name in enumerate(clients.name, 1):
 		print(f"{x}.{name}")
-	com_Id = clients.comId[int(input("Select the community >> "))-1]
-	sub_client =  aminofix.SubClient(comId=com_Id, profile=client.profile)
+	com_id = clients.comId[int(input("-- Select the community::: ")) - 1]
+	sub_client = amino.SubClient(comId=com_id, profile=client.profile)
 	while True:
 		try:
 			sub_client.post_blog(title=title, content=content)
-			print("Created Blog...")
-		except aminofix.lib.util.exceptions.VerificationRequired as e:
-			print("VerificationRequired")
-			verification_link = e.args[0]["url"]; print(verification_link)
-			verification = input("Waiting for verification >> ")
-		except Exception as e:	print(e)
+			print("-- Blog is created!")
+		except amino.lib.util.exceptions.VerificationRequired as e:
+			print(f"-- VerificationRequired::: {e.args[0]['url]}")
+			verification = input("-- Press enter after verification! ")
+		except Exception as e:
+			print(e)
 
 def spam_wikis(title: str, content: str):
-	client = aminofix.Client()    
-	client.login(email=input("Email >> "), password=input("Password >> "))
+	client = amino.Client()    
+	email = input("-- Email::: ")
+	password = input("-- Password::: ")
+	client.login(email=email, password=password)
 	clients =  client.sub_clients(start=0, size=1000)
 	for x, name in enumerate(clients.name, 1):
 		print(f"{x}.{name}")
-	com_Id = clients.comId[int(input("Select the community >> "))-1]
-	sub_client =  aminofix.SubClient(comId=com_Id, profile=client.profile)
+	com_id = clients.comId[int(input("-- Select the community::: ")) - 1]
+	sub_client = amino.SubClient(comId=com_id, profile=client.profile)
 	while True:
 		try:
 			sub_client.post_wiki(title=title, content=content)
-			print("Created Wiki...")
-		except aminofix.lib.util.exceptions.VerificationRequired as e:
-			print("VerificationRequired")
-			verification_link = e.args[0]["url"]; print(verification_link)
-			verification = input("Waiting for verification >> ")
-		except Exception as e:	print(e)
+			print("-- Wiki is created!")
+		except amino.lib.util.exceptions.VerificationRequired as e:
+			print(f"-- VerificationRequired::: {e.args[0]['url]}")
+			verification = input("-- Press enter after verification! ")
+		except Exception as e:
+			print(e)
