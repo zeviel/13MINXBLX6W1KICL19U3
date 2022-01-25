34b4f0bf88b4440a98aaed798e1a2f1a8cb9c56e
deluvsushi <77536370+deluvsushi@users.noreply.github.com>
2022-01-25 13:39:05 +0600

    Update AminoBlogWikiClique.py

--- AminoBlogWikiClique.py
+++ AminoBlogWikiClique.py
@@ -1,18 +1,18 @@
-import pyfiglet
-from tabulate import tabulate
-from configs import main_functions, menu_configs
 from sty import fg
-print(fg(110))
-print("""Script by DeLuvSushi
+from tabulate import tabulate
+from pyfiglet import figlet_format
+from configs import main_functions, menu_configs
+print(f"""{fg(110)}
+Script by DeLuvSushi
 Github : https://github.com/deluvsushi""")
-print(pyfiglet.figlet_format("aminoblogwikiclique", font="slant", width=55))
+print(figlet_format("aminoblogwikiclique", font="slant", width=55))
 print(tabulate(menu_configs.main_menu, tablefmt="fancy_grid"))
-select = input("Select >> ")
+select = int(input("-- Select::: "))
 
-if select == "1":
-	title = input("Blogs Title >> "); content = input("Blogs Content >> ")
+if select == 1:
+	title = input("-- Blogs Title::: "); content = input("-- Blogs Content::: ")
 	main_functions.spam_blogs(title=title, content=content)
 
-elif select =="2":
-	title = input("Wiki's Title >> "); content = input("Wiki's Content >> ")
+elif select == 2:
+	title = input("-- Wiki's Title::: "); content = input("-- Wiki's Content::: ")
 	main_functions.spam_wikis(title=title, content=content)
